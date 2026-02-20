34db4082913039099487e437ad898a42a226a424
Jooja <joojabot@proton.me>
2026-02-20 16:10:56 +0400

    Add Apple PWA meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,10 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      meta: [
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }
+      ],
       link: [
         { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png' }
       ]
