4c14339dca77a0f424e05609016d2ae6350f9f84
powerdot <donpsix@gmail.com>
2026-02-23 02:05:38 +0400

    Create app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  ui: {
+    input: {
+      defaultVariants: {
+        size: 'xl'
+      }
+    },
+    textarea: {
+      defaultVariants: {
+        size: 'xl'
+      }
+    }
+  }
+})
