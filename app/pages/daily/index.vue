77777277cc592abb7a288c46ef1697324b810102
powerdot <donpsix@gmail.com>
2026-03-06 02:55:14 +0400

    daily page init

---
+++ app/pages/daily/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+onMounted(()=>{
+
+})
+
+onUnmounted(() => {
+
+})
+</script>
+
+<template>
+  <main class="wrap">
+    <UPageHeader title="Daily Trainer"/>
+    <UPageBody>
+      <UCard variant="subtle">
+        
+      </UCard>
+    </UPageBody>
+  </main>
+</template>
+
+<style scoped lang="scss">
+
+</style>
